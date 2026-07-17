a11c75f629b006b2296ecf1b75a0acdd93f4f084
Ozy Prazuganda <OzyPrazuganda75@gmail.com>
2026-07-17 17:39:38 +0700

    update on weekly calibration data

--- pages/7_Hotline_Calibration.py
+++ pages/7_Hotline_Calibration.py
@@ -541,7 +541,7 @@ if selected_date not in meeting_data:
     st.stop()
 
 # Date filter
-manual_order = ['Azer', 'Reza', 'Neneng', 'Aulia']
+manual_order = ['Aulia', 'Azer', 'Neneng', 'Reza']
 agent_list = [agent for agent in manual_order if agent in {entry['agent'] for entry in meeting_data[selected_date]}]
 selected_agent = st.sidebar.radio('Agent Sampling', agent_list)
 
